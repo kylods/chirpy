92decbc2d919e5a63da7aba2fc6d2cca1e1140d4
Kyle Dvorak <kuelos@localhost.localdomain>
2023-10-31 02:17:38 -0500

    Base webserver

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/kylods/chirpy
+
+go 1.21.3
---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func middlewareCors(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "*")
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
+}
+
+func main() {
+	h1 := func(w http.ResponseWriter, request *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(200)
+		w.Write([]byte("OK"))
+	}
+
+	mux := http.NewServeMux()
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	mux.HandleFunc("/healthz", h1)
+	corsMux := middlewareCors(mux)
+
+	srv := &http.Server{
+		Addr:    ":8080",
+		Handler: corsMux,
+	}
+
+	log.Fatal(srv.ListenAndServe())
+
+}
